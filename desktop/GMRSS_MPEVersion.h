678344c3c2487193c50d857753a389e76cfb1ad1
zguo <zguo@geomar.de>
2021-08-02 15:31:28 +0200

    对代码进行了优化,核心代码的重复使用和方便维护

--- desktop/GMRSS_MPEVersion.h
+++ desktop/GMRSS_MPEVersion.h
@@ -14,9 +14,9 @@
 #define GMRSS_MPE_MAJOR_VERSION 1
 #define GMRSS_MPE_MINOR_VERSION 0
 #define GMRSS_MPE_PATCH_VERSION 1
-#define GMRSS_MPE_EXTRA_VERSION "-git-e79cba6"
-#define GMRSS_MPE_VERSION       "1.0.1-git-e79cba6"
-#define GMRSS_MPE_DATE          "2021-08-01"
+#define GMRSS_MPE_EXTRA_VERSION "-git-6e84637"
+#define GMRSS_MPE_VERSION       "1.0.1-git-6e84637"
+#define GMRSS_MPE_DATE          "2021-08-02"
 #define GMRSS_MPE_HOST          "zhikuis-MacBook-Pro.local"
 #define GMRSS_MPE_PACKAGER      "zguo"
 #define GMRSS_MPE_OS            "MacOSX"
